9f92e3415b7e3b989f0f51b3fe05bdd25171eee6
yaoshanliang <1329517386@qq.com>
2014-11-09 17:46:20 +0800

    update ajax function

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -5,33 +5,37 @@
  * @Function:
  */
 function testAjax (url, method, content) {
-    //测试
-    var xmlhttprequest = getXmlHttpRequestObject();
-    var method = arguments[1] ? arguments[1] : "POST";
+    var xmlhttprequest = getXmlHttpRequestObject();//实例XMLHttpRequest对象
+/*    var method = arguments[1] ? arguments[1] : "POST";
     var method = (typeof method !== 'undefined') ? method : "POST";
     var method = method || "POST";
-    var content = arguments[2] ? arguments[2] : null;
+    var content = arguments[2] ? arguments[2] : null;*/
 
-    xmlhttprequest.onreadystatechange = onReadyStateChange;
+    xmlhttprequest.onreadystatechange = onReadyStateChange;//状态改变触发函数
     xmlhttprequest.open(method, url, true);
-    if (method == "POST") {//POST方法时候需要设置Content-Type
+    //POST方法时候需要设置Content-Type
+    if (method == "POST") {
         xmlhttprequest.setRequestHeader("Content-Type", "application/x-www-form-urlencode");
-        xmlhttprequest.send(content);
     }
-    xmlhttprequest.send(null);
+    xmlhttprequest.send(content);
 
     function getXmlHttpRequestObject() {
         var xmlhttprequest = null;
-        try {
-            xmlhttprequest = new XMLHttpRequest();//chrome等浏览器
-        } catch(e) {
+        //非IE
+        if(window.XMLHttpRequest) {
+            xmlhttprequest = new XMLHttpRequest();
+        } else if(window.ActiveXObject) {
             try {
-                xmlhttprequest = new ActiveXObject("Msml2.XMLHTTP");//IE
-            } catch (e) {
+                xmlhttprequest = new ActiveXObject("Msml2.XMLHTTP.3.0");
+            } catch(e) {
                 try {
-                    xmlhttprequest = new ActiveXObject("Microsoft.XMLHTTP")//IE
-                } catch (e) {
-                    return false;
+                    xmlhttprequest = new ActiveXObject("Msml2.XMLHTTP.6.0");
+                } catch(e) {
+                    try {
+                        xmlhttprequest = new ActiveXObject("Microsoft.XMLHTTP");
+                    } catch(e) {
+                        return;
+                    }
                 }
             }
         }
@@ -41,24 +45,12 @@ function testAjax (url, method, content) {
 
     //指定状态改变时触发的事件
     function onReadyStateChange() {
-        stateChange();
+        //请求已发送，并且请求成功
+        if (xmlhttprequest.readyState == 4 && xmlhttprequest.status == 200) {
+            alert(xmlhttprequest.responseText);
+        }
     }
 
-    //
-    function stateChange() {
-        alert(xmlhttprequest.readyState);//可以发现状态一次为1：正在加载，2：已加载，3：交互中，4：完成
-        /*if (xmlhttprequest.readyState == 4) {//已发送请求
-            if (xmlhttprequest.status == 200) {
-                //请求成功
-                //var result = getRespose();
-                alert("请求成功！");
-            }
-            alert("发送失败！")
-        } else {
-            //请求失败
-            alert("请求失败！")
-        }*/
-    }
 
     //获取返回结果
     function getResponse(type) {
@@ -67,4 +59,9 @@ function testAjax (url, method, content) {
     }
 
 
+}
+
+function check() {
+    //do some check
+    return CHECK通过 ? true : false;
 }
