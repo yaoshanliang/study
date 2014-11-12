c08b4cb2e67e3974222e68cbc7f15b6eb3464393
yaoshanliang <1329517386@qq.com>
2014-11-12 22:57:23 +0800

    add jquery

---
+++ php/cli/test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Author: iat <iat.net.cn@gmail.com>
+ * @Date: 2014-11-12
+ * @File: test.php
+ * @Function: 
+ */
+/*$YOU =  "YOU" and $I = "I";
+define($I, $YOU);
+echo $I;
+
+echo $YOU === $I ? "LOVE" : $I = $YOU;*/
+
+define("I", "I") and define("YOU", "YOU");
+define(I, YOU);
+//I = YOU;
+//echo YOU === I ? "LOVE" : I = YOU;
