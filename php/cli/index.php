16ee56eea32d985d633bb5649dfefaadb10b0c3e
yaoshanliang <1329517386@qq.com>
2015-03-27 18:10:32 +0800

    update cli

--- php/cli/index.php
+++ php/cli/index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: iat
- * Date: 10/3/14
- * Time: 8:54 PM
- */
 //ask for input
 fwrite(STDOUT, "Enter your name: ");
 
@@ -15,21 +9,19 @@ $name = trim(fgets(STDIN));
 fwrite(STDOUT, "Hello, $name!");
 print_r($argv);
 
-/**
- * 提示用户输入，类似Python
- */
+//提示用户输入，类似Python
 
 $fs = true;
 
 do{
-    if($fs){
-        fwrite(STDOUT,'请输入您的博客名：');
-        $fs = false;
-    }else{
-        fwrite(STDOUT,'抱歉，博客名不能为空，请重新输入您的博客名：');
-    }
+	if($fs){
+		fwrite(STDOUT,'请输入您的博客名：');
+		$fs = false;
+	}else{
+		fwrite(STDOUT,'抱歉，博客名不能为空，请重新输入您的博客名：');
+	}
 
-    $name = trim(fgets(STDIN));
+	$name = trim(fgets(STDIN));
 
 }while(!$name);
 
