f5439c5aad74d5a9f878ddf158d7b4c04315e70b
yaoshanliang <1329517386@qq.com>
2015-05-07 09:18:15 +0800

    change class to function

--- php/sort/index.php
+++ php/sort/index.php
@@ -1,185 +1,127 @@
 <?php
-// |-----------------------------------------------------------------------------------
-// | Author    iat <iat.net.cn@gmail.com>
-// | Site      http://iat.net.cn
-// | Copyright (c) 2013-2014
-// |-----------------------------------------------------------------------------------
-/**
- * hello iat
- * @package
- * @author iat <iat.net.cn@gmail.com>
- * @time
- */
-
-?>
-
-<?php
-class iat
+//交换两数
+function swap(&$a, &$b)
 {
-    /**
-     * swap two datas
-     * @param  [type] $a
-     * @param  [type] $b
-     * @return [type]
-     */
-    public static function swap(&$a, &$b)
-    {
-        $temp = $a;
-        $a = $b;
-        $b = $temp;
-    }
+	$temp = $a;
+	$a = $b;
+	$b = $temp;
+}
 
-    /**
-     * sort by maopao
-     * @param  [type] $arr
-     * @return [type]
-     */
-    public static function sort_by_mp(&$arr)
-    {
-        $n = count($arr);
-        for ($i = 0; $i < $n; $i++)
-        {
-            for ($j = 0; $j < $n -$i - 1; $j++)
-            {
-                if($arr[$j] < $arr[$j + 1])
-                {
-                    self::swap($arr[$j], $arr[$j+1]);
-                }
-            }
-        }
-        var_dump($arr);
-    }
-
-    /**
-     * sort by jiaohuan
-     * @param  [type] $arr
-     * @return [type]
-     */
-    public static function sort_by_jh($arr)
-    {
-        $n = count($arr);
-        for ($i = 0; $i < $n; $i++)
-        {
-            for ($j = $i + 1; $j < $n; $j++)
-            {
-                if($arr[$i] < $arr[$j])
-                {
-                    swap($arr[$i], $arr[$j]);
-                }
-            }
-        }
-        var_dump($arr);
-    }
-
-    /**
-     * sort by xuanze
-     * @param  [type] $arr
-     * @return [type]
-     */
-    public static function sort_by_xz($arr)
-    {
-        $n = count($arr);
-        for ($i = 0; $i < $n ; $i++)
-        {
-            $max = $arr[$i];
-            for ($j = $i + 1; $j < $n ; $j++)
-            {
-                if($max < $arr[$j])
-                {
-                    $max = $arr[$j];
-                }
-            }
-            $arr[$i] = $max;
-        }
-        var_dump($arr);
-    }
-
-    /**
-     * sort by kuaisu
-     * @param  [type] $arr
-     * @return [type]
-     */
-    public static function sort_by_ks($arr)
-    {
-        $n = count($arr);
-        if ($n <= 1)
-        {
-            return $arr;
-        }
-        $left_arr = array();
-        $right_arr = array();
-        $key = $arr[0];
-        for ($i = 1; $i < $n; $i++)
-        {
-            if($key < $arr[$i])
-            {
-                $left_arr[] = $arr[$i];
-            }
-            else
-            {
-                $right_arr[] = $arr[$i];
-            }
-        }
-        $left_arr = self::sort_by_ks($left_arr);
-        $right_arr = self::sort_by_ks($right_arr);
-        return array_merge($left_arr,array($key),$right_arr);
-    }
-
-	//插入排序, 时间复杂度O(n^2)
-	public static function sort_by_cr(&$arr)
+//输出
+function cout($arr)
+{
+	foreach($arr as $a)
 	{
-		$n = count($arr);
-		for($i = 1; $i < $n; $i++)
+		echo $a . " ";
+	}
+	echo "\n";
+}
+
+//冒泡排序
+function sort_by_mp(&$arr)
+{
+	$n = count($arr);
+	for ($i = 0; $i < $n; $i++)
+	{
+		for ($j = 0; $j < $n - $i - 1; $j++)
 		{
-			$j = $i;
-			while($j > 0 && $arr[$i] < $arr[$j - 1])
+			if($arr[$j] < $arr[$j + 1])
 			{
-				$arr[$j] = $arr[$j - 1];
-				$j--;
+				swap($arr[$j], $arr[$j+1]);
 			}
-			$arr[$j] = $arr[$i];
 		}
+	}
+	return $arr;
+}
+
+//交换排序
+function sort_by_jh($arr)
+{
+	$n = count($arr);
+	for ($i = 0; $i < $n; $i++)
+	{
+		for ($j = $i + 1; $j < $n; $j++)
+		{
+			if($arr[$i] < $arr[$j])
+			{
+				swap($arr[$i], $arr[$j]);
+			}
+		}
+	}
+	var_dump($arr);
+}
+
+//选择排序
+function sort_by_xz($arr)
+{
+	$n = count($arr);
+	for ($i = 0; $i < $n ; $i++)
+	{
+		$max = $arr[$i];
+		for ($j = $i + 1; $j < $n ; $j++)
+		{
+			if($max < $arr[$j])
+			{
+				$max = $arr[$j];
+			}
+		}
+		$arr[$i] = $max;
+	}
+	var_dump($arr);
+}
+
+//快速排序，时间复杂度O(nlogn)
+function sort_by_ks($arr)
+{
+	$n = count($arr);
+	if ($n <= 1)
+	{
 		return $arr;
 	}
+	$left_arr = array();
+	$right_arr = array();
+	$key = $arr[0];
+	for ($i = 1; $i < $n; $i++)
+	{
+		if($key < $arr[$i])
+		{
+			$left_arr[] = $arr[$i];
+		}
+		else
+		{
+			$right_arr[] = $arr[$i];
+		}
+	}
+	$left_arr = sort_by_ks($left_arr);
+	$right_arr = sort_by_ks($right_arr);
+	return array_merge($left_arr,array($key),$right_arr);
+}
 
-    /**
-     * find by erfen
-     * @param  [type] $arr
-     * @param  [type] $low
-     * @param  [type] $high
-     * @param  [type] $key
-     * @return [type]
-     */
-    public static function find_by_ef($arr, $low, $high, $key)
-    {
-        if($low <= $high)
-        {
-            $mid = ($low + $high) / 2;
-            if($arr[$mid] == $key)
-            {
-                echo 'find', $key;
-                return $mid;
-            }
-            else if($arr[$mid] > $key)
-            {
-                return self::find_by_ef($arr, $mid + 1, $high, $key);
-            }
-            else
-            {
-                return self::find_by_ef($arr, $low, $mid - 1, $key);
-            }
-        }
-
-        die('not find');
-    }
+//插入排序, 时间复杂度O(n^2)
+function sort_by_cr(&$arr)
+{
+	$n = count($arr);
+	for($i = 1; $i < $n; $i++)
+	{
+		$j = $i;
+		$temp = $arr[$i];
+		while($j > 0 && $temp < $arr[$j - 1])
+		{
+			$arr[$j] = $arr[$j - 1];
+			$j--;
+		}
+		$arr[$j] = $temp;
+	}
+	return $arr;
 }
 
 
-
+//测试====================================================
 $arr = array(1, 3, 2, 5, 6, 3, 3, 1, 4, 5);
+cout($arr);
 // iat::sort_by_mp($arr);
 // iat::sort_by_jh($arr);
 // iat::sort_by_xz($arr);
 // var_dump(iat::sort_by_ks($arr));
-iat::sort_by_cr($arr);
-print_r($arr);
+sort_by_cr($arr);
+cout($arr);
