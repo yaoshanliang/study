bc1f8f5b86590bd573dd1dee58b308ec6ac153d1
yaoshanliang <1329517386@qq.com>
2015-10-19 10:14:16 +0800

    add test form

---
+++ php/form/test.php
@@ -0,0 +1,7 @@
+<form action="http://money.finance.sina.com.cn/tousu/view/Image.php?user_company=2" method="post"
+enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="image" id="" />
+<br />
+<input type="submit" name="submit" value="Submit" />
+</form>
