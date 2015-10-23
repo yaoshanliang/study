79d70ddcfef568f7c32f6ba90e2049dcb01472aa
yaoshanliang <1329517386@qq.com>
2015-10-23 14:23:23 +0800

    test

--- php/curl/baidu.php
+++ php/curl/baidu.php
@@ -1,14 +1,15 @@
 <?php
-while(1) {
 $content = file_get_contents('http://tieba.baidu.com/f?kw=%E7%99%BE%E5%BA%A6%E4%BA%91');
 $url = '/http:\/\/pan.baidu.com\/mbox\/homepage\?short=.{7}/';
 preg_match_all($url, $content, $url_all);
 
-// var_dump($url_all);
 if(!empty($url_all[0])) {
 	foreach($url_all[0] as $v) {
+		if($pos = strpos($v, '<')) {
+			$v = substr($v, 0, $pos);
+		}
 		echo $v, "\n";
 	}
-	// print_r($url_all);
 }
-}
+echo '.';
+
