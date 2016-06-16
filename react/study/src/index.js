650052d43418b9c7c8e1adef7f11a174c9e46a2e
yaoshanliang <1329517386@qq.com>
2016-06-16 11:16:09 +0800

    add ajax request

--- react/study/src/index.js
+++ react/study/src/index.js
@@ -51,17 +51,37 @@ var CommentForm = React.createClass({
 
 // tutorial1.js
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
   }
 });
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="src/comment-data.json" pollInterval={2000}/>,
   document.getElementById('content')
 );
