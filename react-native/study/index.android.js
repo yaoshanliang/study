b04724edacd70f694ba8b389857529524ff308c7
yaoshanliang <1329517386@qq.com>
2016-06-28 16:52:06 +0800

    update

--- react-native/study/index.android.js
+++ react-native/study/index.android.js
@@ -23,327 +23,67 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 
-var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
-var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
-var PAGE_SIZE = 25;
-var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
-var REQUEST_URL = API_URL + PARAMS;
+// var MovieScreen = require('./MovieScreen');
+import SearchScreen from './SearchScreen';
 
-class SampleAppMovies extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
+var _navigator;
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+    _navigator.pop();
+    return true;
   }
+  return false;
+});
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  }
-
-  render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
+var RouteMapper = function(route, navigationOperations, onComponentRef) {
+  _navigator = navigationOperations;
+  if (route.name === 'search') {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderMovie}
-        style={styles.listView}
-      />
+      <SearchScreen navigator={navigationOperations} />
     );
-  }
-
-  renderLoadingView() {
+  } else if (route.name === 'movie') {
     return (
-      <View style={styles.container}>
-        <Text>
-          Loading movies!!!!...
-        </Text>
-      </View>
-    );
-  }
-
-  renderMovie(movie) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
+      <View style={{flex: 1}}>
+        <ToolbarAndroid
+          actions={[]}
+          navIcon={require('image!android_back_white')}
+          onIconClicked={navigationOperations.pop}
+          style={styles.toolbar}
+          titleColor="white"
+          title={route.movie.title} />
+        <MovieScreen
+          style={{flex: 1}}
+          navigator={navigationOperations}
+          movie={route.movie}
         />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
       </View>
     );
   }
 };
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
-});
-var SampleApp = React.createClass({
-
-    configureScene(route){
-      return Navigator.SceneConfigs.FadeAndroid;
-    },
-
-    renderScene(router, navigator){
-      var Component = null;this._navigator = navigator;
-      switch(router.name){
-        case "welcome":
-          Component = WelcomeView;
-          break;
-        case "feed":
-          Component = FeedView;
-          break;
-        default: //default view
-          Component = DefaultView;
-      }
-
-      return <Component navigator={navigator} />
-    },
-
-    componentDidMount() {
-      var navigator = this._navigator;
-      BackAndroid.addEventListener('hardwareBackPress', function() {
-          if (navigator && navigator.getCurrentRoutes().length > 1) {
-            navigator.pop();
-            return true;
-          }
-          return false;
-      });
-    },
-
-
-    componentWillUnmount() {
-      BackAndroid.removeEventListener('hardwareBackPress');
-    },
-
-    render() {
-        return (
-            <Navigator
-                initialRoute={{name: 'welcome'}}
-                configureScene={this.configureScene}
-                renderScene={this.renderScene} />
-        );
-    }
-
-});
-var FeedView = React.createClass({
-    goBack(){
-      this.props.navigator.push({name:"default"});
-    },
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome} onPress={this.goBack} >
-                    I am Feed View! Tab to default view!
-                </Text>
-            </View>
-        )
-    }
-});
-
-
-var WelcomeView = React.createClass({
-    onPressFeed() {
-        this.props.navigator.push({name: 'feed'});
-    },
-
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome} onPress={this.onPressFeed} >
-                    This is welcome view.Tap to go to feed view.
-                </Text>
-            </View>
-        );
-    }
-
-});
-
-var DefaultView = React.createClass({
-
-    render(){
-      return (
-          <View style={styles.container}>
-              <Text style={styles.welcome}>Default view</Text>
-          </View>
-      )
-    }
-});
-const Row = React.createClass({
-  _onClick: function() {
-    this.props.onClick(this.props.data);
-  },
-  render: function() {
-    return (
-     <TouchableWithoutFeedback onPress={this._onClick} >
-        <View style={styles.row}>
-          <Text style={styles.text}>
-            {this.props.data.text + ' (' + this.props.data.clicks + ' clicks)'}
-          </Text>
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  },
-});
-
-const RefreshControlExample = React.createClass({
-  statics: {
-    title: '<RefreshControl>',
-    description: 'Adds pull-to-refresh support to a scrollview.'
-  },
-
-  getInitialState() {
-    return {
-      isRefreshing: false,
-      loaded: 0,
-      rowData: Array.from(new Array(20)).map(
-        (val, i) => ({text: 'Initial row' + i, clicks: 0})),
-    };
-  },
-
-  _onClick(row) {
-    row.clicks++;
-    this.setState({
-      rowData: this.state.rowData,
-    });
-  },
-
+class MoviesApp extends React.Component {
   render() {
-    const rows = this.state.rowData.map((row, ii) => {
-      return <Row key={ii} data={row} onClick={this._onClick}/>;
-    });
+    var initialRoute = {name: 'search'};
     return (
-      <ScrollView
-        style={styles.scrollview}
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.isRefreshing}
-            onRefresh={this._onRefresh}
-            tintColor="#ff0000"
-            title="Loading..."
-            colors={['#ff0000', '#00ff00', '#0000ff']}
-            progressBackgroundColor="#ffffff"
-          />
-        }>
-        {rows}
-      </ScrollView>
-    );
-  },
-
-  _onRefresh() {
-    this.setState({isRefreshing: true});
-    setTimeout(() => {
-      // prepend 10 items
-      const rowData = Array.from(new Array(10))
-      .map((val, i) => ({
-        text: 'Loaded row' + (+this.state.loaded + i),
-        clicks: 0,
-      }))
-      .concat(this.state.rowData);
-
-      this.setState({
-        loaded: this.state.loaded + 10,
-        isRefreshing: false,
-        rowData: rowData,
-      });
-    }, 1);
-  },
-});
-
-class ScrollViewComponent extends React.Component {
-    static defaultProps = {
-
-    };
-  constructor(props){
-    super(props);
-      this.state = {
-        isRefreshing: false
-      };
-    }
-  _onRefresh() {
-    this.setState({isRefreshing: true});
-    setTimeout(() => {
-      // prepend 10 items
-      const rowData = Array.from(new Array(10))
-      .map((val, i) => ({
-        text: 'Loaded row' + (+this.state.loaded + i),
-        clicks: 0,
-      }))
-      .concat(this.state.rowData);
-
-      this.setState({
-        loaded: this.state.loaded + 10,
-        isRefreshing: false,
-        rowData: rowData,
-      });
-    }, 5000)
-  }
-  render() {
-    return (
-      <ScrollView
-        style={styles.scrollview}
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.isRefreshing}
-            onRefresh={()=>this._onRefresh}
-            tintColor="#ff0000"
-            title="Loading..."
-            colors={['#ff0000', '#00ff00', '#0000ff']}
-            progressBackgroundColor="#ffffff"
-          />
-        }>
-        <Text>111</Text>
-      </ScrollView>
+      <Navigator
+        style={styles.container}
+        initialRoute={initialRoute}
+        configureScene={() => Navigator.SceneConfigs.FadeAndroid}
+        renderScene={RouteMapper}
+      />
     );
   }
 }
 
-AppRegistry.registerComponent('study', () => ScrollViewComponent);
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  toolbar: {
+    backgroundColor: '#a9a9a9',
+    height: 56,
+  },
+});
+
+AppRegistry.registerComponent('study', () => MoviesApp);
