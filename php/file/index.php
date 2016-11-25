6390a764f3070006144bcfb6de0f9950918ad23a
yaoshanliang <1329517386@qq.com>
2016-11-25 17:11:55 +0800

    add update word

--- php/file/index.php
+++ php/file/index.php
@@ -1,30 +1,29 @@
 <?php
-/**
- * 
- * @Author: iat <iat.net.cn@gmail.com>
- * @Date: 2014-10-28
- * @File: index.php
- * @Function: compare file_put_contents with fwrite
- */
 
-$filename = 'file.txt';
-$content = 'hello world 123';
-file_put_contents($filename, $content . PHP_EOL, FILE_APPEND);//PHP_EOL表示当前系统的换行符，FILE_APPEND表示追加
+$filename = 'word.txt';
+$newfilename = 'newword.txt';
 
-$file = fopen($filename, 'w');//r r+ W w+ a a+ X x+类型
-fwrite($file, $content);
-fclose($file);
+$word = file_get_contents($filename);
 
 
-/**
- * @name abd
- * @param $a
- * @param $b
- * @return int
- * @function
- */
-function abd($a, $b) {
+$wordarr = explode("\n", $word);
 
-    return 21;
+foreach ($wordarr as &$v) {
+    $blank = str_pad('', 30 - strpos($v, '['));
+    $v = str_replace('[', $blank . '[', $v);
 }
+
+// print_r($wordarr);
+
+$newword = implode("\n", $wordarr);
+
+// echo $newword;
+$newword = preg_replace('/\[(.*)\]/', '', $newword);
+echo $newword;
+
+
+
+file_put_contents($newfilename, $newword);
+
+// echo $word;
 
